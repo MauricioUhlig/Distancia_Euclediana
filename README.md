eca758ebba008c890519fded4c92983d00371e31
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2019-06-08 12:18:19 -0300

    Working

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Distancia Euclediana
+# Distância Euclediana
 
 Projeto desenvolvido para um trabaho da disciplina de Complexidade de Algorítmos
 
@@ -12,9 +12,9 @@ Para solução do problema foi utilizado a técnica de *Divisão e Conquista* at
 
 ### Referências
 
-- [Youtube](https://www.youtube.com/watch?v=NtKjWkcHQrk&app=desktop)
-- [PDF](http://www.decom.ufop.br/toffolo/site_media/uploads/2011-1/bcc402/slides/08._divisao_e_conquista.pdf)
-
+- Youtube: [Algoritmo - Menor Distância entre Pontos no Plano 2D](https://www.youtube.com/watch?v=NtKjWkcHQrk&app=desktop)
+- PDF: [divisao_e_conquista.pdf](http://www.decom.ufop.br/toffolo/site_media/uploads/2011-1/bcc402/slides/08._divisao_e_conquista.pdf)
+- PDF: [Par_Mais_Proximo.pdf](http://sweet.ua.pt/leslie/Geocomp/Slides/GC_09_10_3_Par_Mais_Proximo.pdf)
 # ...
 ``` 
 print("Que a força esteja com você!!!")
--- distancia.py
+++ distancia.py
@@ -1,70 +1,81 @@
 #distancia euclediana 
 
-# v1 = [1.2, 2, 3.8, 4.5]
-# v2 = [0.5, 4.5, 9.6, 3.4]
-
-# def dist_euclidiana(v1, v2):
-# 	dim, soma = len(v1), 0
-# 	for i in range(dim):
-# 		soma += math.pow(v1[i] - v2[i], 2)
-# 	return math.sqrt(soma)
-
-# print('%.2f' % dist_euclidiana(v1, v2))
-
-# print("teste de python")
-
 import math
 
 m = [[8, 9],[1, 3], [7, 8], [9, 2], [3, 10], [10, 1], [2, 4], [6, 6], [11, 12], [4, 5], [12, 11], [5, 7]]
 
-print "antes"
-print m
+# print "antes"
+# print m
 
-print "depois"
-def sort(array):
+def sort(array, axis):
     for p in range(0, len(array)):
         current_elementX = array[p][0]
         current_elementY = array[p][1]
 
-        while p > 0 and array[p -1][0] > current_elementX:
-            array[p][0] = array[p -1][0]
-            array[p][1] = array[p -1][1]
-            p -= 1
-
+        if(axis == "X" or axis == "x"):
+            while p > 0 and array[p -1][0] > current_elementX:
+                array[p][0] = array[p -1][0]
+                array[p][1] = array[p -1][1]
+                p -= 1
+        elif(axis == "Y" or axis == "y"):
+            while p > 0 and array[p -1][1] > current_elementY:
+                array[p][0] = array[p -1][0]
+                array[p][1] = array[p -1][1]
+                p -= 1
         array[p][0] = current_elementX
         array[p][1] = current_elementY
 
-sort(m)
-print m
+sort(m, "x")
+
+# print "depois"
+# print m
 
 def dist(a,b):
     return math.sqrt(math.pow(a[0] - b[0],2) + math.pow(a[1]-b[1],2))
 
 def minDistBrute(array):
-    return min(dist(array[0],array[1]),dist(array[1],array[2]),dist(array[2],array[0]))
+    if(len(array)) == 3:
+        return min(dist(array[0],array[1]),dist(array[1],array[2]),dist(array[2],array[0]))
+    else: 
+        return dist(array[0],array[1])
 
-def minDist(array, n):
-    if n <= 3:
-        return minDistBrute(array)
+def minDistFront(s, end, lower):
+    sort(s,"y")
+    
+    for i in range(0, len(s)-1):
+        j = i+1
+        while (j < end) and (s[j][1]-s[i][1] < lower):
+            aux = dist(s[i],s[j])
+            j += 1
+            if( aux < lower):
+                lower = aux
+    return lower
 
-    m = n/2
-    dl = minDist(array, m)
-    dr = minDist(array+m, n-m)
+def minDist(array, start, end):
+    print "Chamada recursiva: start->  "+ str(start)+ " end-> " + str(end)
+    if (end - start) <= 3:
+        new = []
+        for i in range(start, end):
+            new.append(array[i])
+        print new, minDistBrute(new)
+        return minDistBrute(new)
+
+    middle = int((start + end)/2)
+    dl = minDist(array, start, middle)
+    dr = minDist(array, middle, end)
     d = min(dr,dl)
     s = []
-    midx = array[m][0]
+    midx = array[middle][0]
     j = 0
 
-    for i in range(0, n):
+    for i in range(start, end):
         if math.sqrt(math.pow(array[i][0]-midx,2)) < d:
-            s.append(array[i][0])
+            s.append(array[i])
             j = j+1
     mf = minDistFront(s, j, d)
     return min(d, mf)
 
 
-
-
-
-resp = minDistBrute(m)
+print m
+resp = minDist(m, 0, len(m)-1)
 print resp
