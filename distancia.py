cabe939e11eaf0cf021e5f31b7b9901b34e3a34b
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2019-06-08 01:08:51 -0300

    minDistBrute

--- distancia.py
+++ distancia.py
@@ -1,7 +1,5 @@
 #distancia euclediana 
 
-# import math
-
 # v1 = [1.2, 2, 3.8, 4.5]
 # v2 = [0.5, 4.5, 9.6, 3.4]
 
@@ -15,30 +13,31 @@
 
 # print("teste de python")
 
+import math
 
-m = [[8, 1, 7, 9, 3, 10, 2, 6,11, 4, 12, 5],[9, 3, 8, 2, 10, 1, 4, 6, 12, 5, 11, 7]]
+m = [[8, 9],[1, 3], [7, 8], [9, 2], [3, 10], [10, 1], [2, 4], [6, 6], [11, 12], [4, 5], [12, 11], [5, 7]]
 
 print "antes"
-print m[0]
-print m[1]
+print m
 
 print "depois"
 def sort(array):
-    for p in range(0, len(array[0])):
-        current_elementX = array[0][p]
-        current_elementY = array[1][p]
+    for p in range(0, len(array)):
+        current_elementX = array[p][0]
+        current_elementY = array[p][1]
 
-        while p > 0 and array[0][p - 1] > current_elementX:
-            array[0][p] = array[0][p - 1]
-            array[1][p] = array[1][p - 1]
+        while p > 0 and array[p -1][0] > current_elementX:
+            array[p][0] = array[p -1][0]
+            array[p][1] = array[p -1][1]
             p -= 1
 
-        array[0][p] = current_elementX
-        array[1][p] = current_elementY
+        array[p][0] = current_elementX
+        array[p][1] = current_elementY
 
 sort(m)
-print m[0]
-print m[1]
+print m
+
+
 
 def minDist(array, n):
     if n <= 3:
@@ -49,12 +48,22 @@ def minDist(array, n):
     dr = minDist(array+m, n-m)
     d = min(dr,dl)
     s = []
-    midx = array[0][m]
+    midx = array[m][0]
     j = 0
 
     for i in range(0, n):
-        if math.sqrt(math.pow(array[0][i]-midx)) < d:
-            s.append(array[0][i])
+        if math.sqrt(math.pow(array[i][0]-midx,2)) < d:
+            s.append(array[i][0])
             j = j+1
     mf = minDistFront(s, j, d)
     return min(d, mf)
+
+def minDistBrute(array):
+    return min(dist(array[0],array[1]),dist(array[1],array[2]),dist(array[2],array[0]))
+
+def dist(a,b):
+    return math.sqrt(math.pow(a[0] - b[0],2) + math.pow(a[1]-b[1],2))
+
+
+resp = minDistBrute(m)
+print resp
